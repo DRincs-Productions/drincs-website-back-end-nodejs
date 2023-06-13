07232ce6e9de3dc89b44f966008028ae2ca6248c
Exodia <67595890+DonRP@users.noreply.github.com>
2023-06-13 21:55:13 +0200

    add limit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import express, { Express } from 'express';
+import rateLimit from 'express-rate-limit';
 import { AuthController } from "./controllers/AuthController";
 import { GitHubController } from "./controllers/GitHubController";
 import { TranslationController } from "./controllers/TranslationController";
@@ -19,6 +20,16 @@ const app: Express = express();
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// add limit
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 try {
     initializeFirebaseApp()
